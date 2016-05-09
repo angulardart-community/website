1c47b002a0b33db1845ff61011cc261d5e2d7f17
Thibault Sottiaux <thso@google.com>
2016-05-09 21:13:45 +0100

    Sync with 1c0218b
    Synced with angular/angular.io master branch, commit 1c0218b:
    https://github.com/angular/angular.io/tree/1c0218bc793200a9e3f0306ea4cea2b7988f7b63/public/docs/_examples/pipes/dart

--- lib/fetch_json_pipe.dart
+++ lib/fetch_json_pipe.dart
@@ -5,14 +5,14 @@ import 'dart:convert';
 import 'package:angular2/angular2.dart';
 
 @Pipe(name: 'fetch', pure: false)
-@Injectable()
 class FetchJsonPipe extends PipeTransform {
   dynamic _fetchedValue;
   Future<dynamic> _fetchPromise;
-  transform(dynamic value, [List<dynamic> args]) {
+
+  transform(dynamic url, [List<dynamic> args]) {
     if (_fetchPromise == null) {
       _fetchPromise = new Future(() async {
-        _fetchedValue = JSON.decode(await HttpRequest.getString(value));
+        _fetchedValue = JSON.decode(await HttpRequest.getString(url));
       });
     }
     return _fetchedValue;
