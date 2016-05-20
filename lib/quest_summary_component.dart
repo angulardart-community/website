ebc048356db84dbf9203688e1bc536a89034e647
Patrice Chalin <pchalin@gmail.com>
2016-05-20 05:07:18 -0700

    Sync with 12984fd
    Synced with angular/angular.io master branch, commit 12984fd:
    https://github.com/angular/angular.io/tree/12984fda55851e6bd53512a287bcb3a84767769b/public/docs/_examples/component-styles/dart

---
+++ lib/quest_summary_component.dart
@@ -0,0 +1,11 @@
+import 'package:angular2/core.dart';
+
+@Component(
+    selector: 'quest-summary',
+    templateUrl: 'quest_summary_component.html',
+    styleUrls: const ['quest_summary_component.css'])
+class QuestSummaryComponent {}
+/*
+  // warning: few browsers support shadow DOM encapsulation at this time
+  encapsulation: ViewEncapsulation.Native
+*/
