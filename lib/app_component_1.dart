d518051a580f399971abb5b07d091a78085da480
Patrice Chalin <pchalin@gmail.com>
2016-06-03 16:45:49 -0700

    Sync with 05864c2
    Synced with angular/angular.io master branch, commit 05864c2:
    https://github.com/angular/angular.io/tree/05864c2584b48b4755e66042b34a6f2b53b89556/public/docs/_examples/toh-4/dart

---
+++ lib/app_component_1.dart
@@ -0,0 +1,37 @@
+import 'package:angular2/core.dart';
+
+import 'hero.dart';
+import 'hero_detail_component.dart';
+import 'hero_service_1.dart';
+
+// Testable but never shown
+@Component(
+    selector: 'my-app',
+    template: '''
+    <div *ngFor="let hero of heroes" (click)="onSelect(hero)">
+      {{hero.name}}
+    </div>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+    ''',
+    directives: const [HeroDetailComponent],
+    providers: const [HeroService])
+class AppComponent implements OnInit {
+  String title = 'Tour of Heroes';
+  List<Hero> heroes;
+  Hero selectedHero;
+
+  HeroService heroService = new HeroService(); // don't do this
+  final HeroService _heroService;
+  AppComponent(this._heroService);
+  getHeroes() {
+    heroes = _heroService.getHeroes();
+  }
+
+  ngOnInit() {
+    getHeroes();
+  }
+
+  onSelect(Hero hero) {
+    selectedHero = hero;
+  }
+}
---
+++ lib/hero_service.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:angular2/core.dart';
+
+import 'hero.dart';
+import 'mock_heroes.dart';
+
+@Injectable()
+class HeroService {
+  Future<List<Hero>> getHeroes() async => mockHeroes;
+  // See the "Take it slow" appendix
+  Future<List<Hero>> getHeroesSlowly() {
+    return new Future.delayed(const Duration(seconds: 2), () => mockHeroes);
+  }
+}
