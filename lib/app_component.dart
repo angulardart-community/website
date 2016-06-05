df44ccabc20e747bcdf03682a2dc31dc2e5dacf6
Patrice Chalin <pchalin@gmail.com>
2016-06-05 11:24:17 -0700

    Sync with c5ff08d
    Synced with angular/angular.io chalin-chore-dart-ex-hosting-0604 branch, commit c5ff08d:
    https://github.com/chalin/angular.io/tree/c5ff08de268de7b5f6584a3c870b374281200869/public/docs/_examples/toh-1/dart

---
+++ lib/app_component.dart
@@ -0,0 +1,23 @@
+import 'package:angular2/core.dart';
+
+class Hero {
+  final int id;
+  String name;
+
+  Hero(this.id, this.name);
+}
+
+@Component(
+    selector: 'my-app',
+    template: '''
+      <h1>{{title}}</h1>
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>name: </label>
+        <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+      </div>''')
+class AppComponent {
+  String title = 'Tour of Heroes';
+  Hero hero = new Hero(1, 'Windstorm');
+}
