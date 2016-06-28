20cf4ad3565d4803301da046fb029efed139e65b
Patrice Chalin <pchalin@gmail.com>
2016-06-28 16:31:44 -0700

    Sync with e5b11d4
    Synced with angular/angular.io master branch, commit e5b11d4:
    https://github.com/angular/angular.io/tree/e5b11d456c31fa2e6879bdf5ba6ce3013d20e237/public/docs/_examples/toh-2/dart

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -7,6 +7,19 @@ class Hero {
   Hero(this.id, this.name);
 }
 
+final List<Hero> mockHeroes = [
+  new Hero(11, 'Mr. Nice'),
+  new Hero(12, 'Narco'),
+  new Hero(13, 'Bombasto'),
+  new Hero(14, 'Celeritas'),
+  new Hero(15, 'Magneta'),
+  new Hero(16, 'RubberMan'),
+  new Hero(17, 'Dynama'),
+  new Hero(18, 'Dr IQ'),
+  new Hero(19, 'Magma'),
+  new Hero(20, 'Tornado')
+];
+
 @Component(
     selector: 'my-app',
     template: '''
@@ -87,16 +100,3 @@ class AppComponent {
     selectedHero = hero;
   }
 }
-
-final List<Hero> mockHeroes = [
-  new Hero(11, 'Mr. Nice'),
-  new Hero(12, 'Narco'),
-  new Hero(13, 'Bombasto'),
-  new Hero(14, 'Celeritas'),
-  new Hero(15, 'Magneta'),
-  new Hero(16, 'RubberMan'),
-  new Hero(17, 'Dynama'),
-  new Hero(18, 'Dr IQ'),
-  new Hero(19, 'Magma'),
-  new Hero(20, 'Tornado')
-];
