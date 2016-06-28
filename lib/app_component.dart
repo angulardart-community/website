7892c93abc15e80dc6e32b4f44d21603c2136a16
Patrice Chalin <pchalin@gmail.com>
2016-06-28 16:40:12 -0700

    Sync with e5b11d4
    Synced with angular/angular.io master branch, commit e5b11d4:
    https://github.com/angular/angular.io/tree/e5b11d456c31fa2e6879bdf5ba6ce3013d20e237/public/docs/_examples/toh-3/dart

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -3,6 +3,19 @@ import 'package:angular2/core.dart';
 import 'hero.dart';
 import 'hero_detail_component.dart';
 
+final List<Hero> mockHeroes = [
+  new Hero(11, 'Mr. Nice'),
+  new Hero(12, 'Narco'),
+  new Hero(13, 'Bombasto'),
+  new Hero(14, 'Celeritas'),
+  new Hero(15, 'Magneta'),
+  new Hero(16, 'RubberMan'),
+  new Hero(17, 'Dynama'),
+  new Hero(18, 'Dr IQ'),
+  new Hero(19, 'Magma'),
+  new Hero(20, 'Tornado')
+];
+
 @Component(
     selector: 'my-app',
     template: '''
@@ -75,20 +88,7 @@ class AppComponent {
   final List<Hero> heroes = mockHeroes;
   Hero selectedHero;
 
-  onSelect(Hero hero) {
+  void onSelect(Hero hero) {
     selectedHero = hero;
   }
 }
-
-final List<Hero> mockHeroes = [
-  new Hero(11, 'Mr. Nice'),
-  new Hero(12, 'Narco'),
-  new Hero(13, 'Bombasto'),
-  new Hero(14, 'Celeritas'),
-  new Hero(15, 'Magneta'),
-  new Hero(16, 'RubberMan'),
-  new Hero(17, 'Dynama'),
-  new Hero(18, 'Dr IQ'),
-  new Hero(19, 'Magma'),
-  new Hero(20, 'Tornado')
-];
--- lib/hero_detail_component.dart
+++ lib/hero_detail_component.dart
@@ -5,15 +5,14 @@ import 'hero.dart';
 @Component(
     selector: 'my-hero-detail',
     template: '''
-    <div *ngIf="hero != null">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-      </div>
-    </div>
-  ''')
+      <div *ngIf="hero != null">
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+          <label>name: </label>
+          <input [(ngModel)]="hero.name" placeholder="name">
+        </div>
+      </div>''')
 class HeroDetailComponent {
   @Input()
   Hero hero;
