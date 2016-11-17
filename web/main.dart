33c273821ddf26383a4d5eed947ab321cdb36448
Patrice Chalin <pchalin@gmail.com>
2016-11-17 05:30:27 -0800

    Sync with 061e50f
    Synced with angular/angular.io master branch, commit 061e50f:
    https://github.com/angular/angular.io/tree/061e50fa065b9e81dbebc2ce4af94dfa98a5942e/public/docs/_examples/dependency-injection/dart

--- web/main.dart
+++ web/main.dart
@@ -1,9 +1,7 @@
 import 'package:angular2/platform/browser.dart';
 
 import 'package:dependency_injection/app_component.dart';
-import 'package:dependency_injection/providers_component.dart';
 
 void main() {
   bootstrap(AppComponent);
-  bootstrap(ProvidersComponent);
 }
