960ab4d01477327fc49c57cb1b87ede3d9d1bf25
Patrice Chalin <pchalin@gmail.com>
2017-01-19 10:05:09 -0800

    Sync with 6711bb3
    Synced with dart-lang/site-webdev chalin-ng-security-guide-1220 branch, commit 6711bb3:
    https://github.com/chalin/site-webdev/tree/6711bb3c366f89a2b34cda6165996f7f1dd0ceaf/public/docs/_examples/security/dart

---
+++ lib/bypass_security_component.dart
@@ -0,0 +1,30 @@
+import 'package:angular2/core.dart';
+
+/// NOTE: this implementation is incomplete. Current content is provided
+/// as a placeholder.
+@Component(
+    selector: 'bypass-security', templateUrl: 'bypass_security_component.html')
+class BypassSecurityComponent {
+  // DomSanitizationService sanitizer;
+  String dangerousUrl;
+  dynamic /*SafeUrl*/ trustedUrl;
+  String dangerousVideoUrl;
+  dynamic /*SafeResourceUrl*/ videoUrl;
+
+  BypassSecurityComponent(/*this.sanitizer*/) {
+    // javascript: URLs are dangerous if attacker controlled.
+    // Angular sanitizes them in data binding, but we can
+    // explicitly tell Angular to trust this value:
+    dangerousUrl = 'javascript:alert("Hi there")';
+    trustedUrl = /*sanitizer.bypassSecurityTrustUrl*/ ('javascript:alert("Hi there")');
+    updateVideoUrl('PUBnlbjZFAI');
+  }
+  void updateVideoUrl(String id) {
+    // Appending an ID to a YouTube URL is safe.
+    // Always make sure to construct SafeValue objects as
+    // close as possible to the input data, so
+    // that it's easier to check if the value is safe.
+    dangerousVideoUrl = 'https://www.youtube.com/embed/$id';
+    videoUrl = /*sanitizer.bypassSecurityTrustResourceUrl*/ (dangerousVideoUrl);
+  }
+}
---
+++ lib/inner_html_binding_component.dart
@@ -0,0 +1,9 @@
+import 'package:angular2/core.dart';
+
+@Component(
+    selector: 'inner-html-binding',
+    templateUrl: 'inner_html_binding_component.html')
+class InnerHtmlBindingComponent {
+  // E.g. a user/attacker controlled value from a URL.
+  var htmlSnippet = 'Template <script>alert("0wned")</script> <b>Syntax</b>';
+}
