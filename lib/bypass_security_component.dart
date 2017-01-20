497a73c45ca087108c78f6023a361defc719fb5e
Patrice Chalin <pchalin@gmail.com>
2017-01-19 19:18:28 -0800

    Sync with ab90c65
    Synced with dart-lang/site-webdev chalin-ng-security-guide-1220 branch, commit ab90c65:
    https://github.com/chalin/site-webdev/tree/ab90c65cfbd2a6e0abcb203e13b9df6e91e1e7c7/public/docs/_examples/security/dart

--- lib/bypass_security_component.dart
+++ lib/bypass_security_component.dart
@@ -1,22 +1,24 @@
 import 'package:angular2/core.dart';
+import 'package:angular2/security.dart';
 
 /// NOTE: this implementation is incomplete. Current content is provided
 /// as a placeholder.
 @Component(
     selector: 'bypass-security', templateUrl: 'bypass_security_component.html')
 class BypassSecurityComponent {
-  // DomSanitizationService sanitizer;
+  DomSanitizationService sanitizer;
   String dangerousUrl;
-  dynamic /*SafeUrl*/ trustedUrl;
+  SafeUrl trustedUrl;
   String dangerousVideoUrl;
-  dynamic /*SafeResourceUrl*/ videoUrl;
+  SafeResourceUrl videoUrl;
 
-  BypassSecurityComponent(/*this.sanitizer*/) {
+  BypassSecurityComponent(this.sanitizer) {
     // javascript: URLs are dangerous if attacker controlled.
     // Angular sanitizes them in data binding, but we can
     // explicitly tell Angular to trust this value:
     dangerousUrl = 'javascript:alert("Hi there")';
-    trustedUrl = /*sanitizer.bypassSecurityTrustUrl*/ ('javascript:alert("Hi there")');
+    trustedUrl =
+        sanitizer.bypassSecurityTrustUrl('javascript:alert("Hi there")');
     updateVideoUrl('PUBnlbjZFAI');
   }
   void updateVideoUrl(String id) {
@@ -25,6 +27,6 @@ class BypassSecurityComponent {
     // close as possible to the input data, so
     // that it's easier to check if the value is safe.
     dangerousVideoUrl = 'https://www.youtube.com/embed/$id';
-    videoUrl = /*sanitizer.bypassSecurityTrustResourceUrl*/ (dangerousVideoUrl);
+    videoUrl = sanitizer.bypassSecurityTrustResourceUrl(dangerousVideoUrl);
   }
 }
--- lib/inner_html_binding_component.dart
+++ lib/inner_html_binding_component.dart
@@ -4,6 +4,6 @@ import 'package:angular2/core.dart';
     selector: 'inner-html-binding',
     templateUrl: 'inner_html_binding_component.html')
 class InnerHtmlBindingComponent {
-  // E.g. a user/attacker controlled value from a URL.
+  // For example, a user/attacker-controlled value from a URL.
   var htmlSnippet = 'Template <script>alert("0wned")</script> <b>Syntax</b>';
 }
