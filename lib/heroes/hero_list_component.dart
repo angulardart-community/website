4a2a0c171a0b8bb72291e0f94b6921d59df2e8fa
Patrice Chalin <pchalin@gmail.com>
2017-05-17 06:17:10 -0400

    Sync with c6e36ab
    Synced with dart-lang/site-webdev master branch, commit c6e36ab:
    https://github.com/dart-lang/site-webdev/tree/c6e36abc78469d9d8d7af73849fcfe29572bc0f2/examples/ng/doc/dependency-injection

--- lib/heroes/hero_list_component.dart
+++ lib/heroes/hero_list_component.dart
@@ -1,15 +1,17 @@
-import 'package:angular2/core.dart';
+import 'package:angular2/angular2.dart';
 
 import 'hero.dart';
 import 'hero_service.dart';
 
 @Component(
-    selector: 'hero-list',
-    template: '''
-      <div *ngFor="let hero of heroes">
-        {{hero.id}} - {{hero.name}}
-        ({{hero.isSecret ? 'secret' : 'public'}})
-      </div>''')
+  selector: 'hero-list',
+  template: '''
+    <div *ngFor="let hero of heroes">
+      {{hero.id}} - {{hero.name}}
+      ({{hero.isSecret ? 'secret' : 'public'}})
+    </div>''',
+  directives: const [CORE_DIRECTIVES],
+)
 class HeroListComponent {
   final List<Hero> heroes;
 
