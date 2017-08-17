bfa848dd1b3e0250ffbbc1a043fe8d0e38ff2010
Patrice Chalin <pchalin@gmail.com>
2017-08-17 16:07:01 -0400

    Sync with 7b91fb7
    Synced with dart-lang/site-webdev 4-dev branch, commit 7b91fb7:
    https://github.com/dart-lang/site-webdev/tree/7b91fb78decd00b545ec12081a6922442cd496a7/examples/ng/doc/toh-3

--- lib/src/hero_detail_component.dart
+++ lib/src/hero_detail_component.dart
@@ -1,4 +1,5 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
 
 import 'hero.dart';
 
@@ -13,7 +14,7 @@ import 'hero.dart';
         <input [(ngModel)]="hero.name" placeholder="name">
       </div>
     </div>''',
-  directives: const [COMMON_DIRECTIVES],
+  directives: const [CORE_DIRECTIVES, formDirectives],
 )
 class HeroDetailComponent {
   @Input()
