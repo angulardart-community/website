64d71ff5720f2602063bae70c795b600dfedaa84
Patrice Chalin <pchalin@gmail.com>
2017-09-07 08:30:49 -0400

    Sync with cd7f99a
    Synced with dart-lang/site-webdev master branch, commit cd7f99a:
    https://github.com/dart-lang/site-webdev/tree/cd7f99aa39ac6efaba8700db8d94d721ba063fce/examples/ng/doc/toh-3

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,5 +1,4 @@
 import 'package:angular/angular.dart';
-import 'package:angular_forms/angular_forms.dart';
 
 import 'src/hero.dart';
 import 'src/hero_detail_component.dart';
@@ -31,61 +30,8 @@ final List<Hero> mockHeroes = [
     </ul>
     <hero-detail [hero]="selectedHero"></hero-detail>
   ''',
-  styles: const [
-    '''
-      .selected {
-        background-color: #CFD8DC !important;
-        color: white;
-      }
-      .heroes {
-        margin: 0 0 2em 0;
-        list-style-type: none;
-        padding: 0;
-        width: 15em;
-      }
-      .heroes li {
-        cursor: pointer;
-        position: relative;
-        left: 0;
-        background-color: #EEE;
-        margin: .5em;
-        padding: .3em 0em;
-        height: 1.6em;
-        border-radius: 4px;
-      }
-      .heroes li.selected:hover {
-        color: white;
-      }
-      .heroes li:hover {
-        color: #607D8B;
-        background-color: #EEE;
-        left: .1em;
-      }
-      .heroes .text {
-        position: relative;
-        top: -3px;
-      }
-      .heroes .badge {
-        display: inline-block;
-        font-size: small;
-        color: white;
-        padding: 0.8em 0.7em 0em 0.7em;
-        background-color: #607D8B;
-        line-height: 1em;
-        position: relative;
-        left: -1px;
-        top: -4px;
-        height: 1.8em;
-        margin-right: .8em;
-        border-radius: 4px 0px 0px 4px;
-      }
-    '''
-  ],
-  directives: const [
-    CORE_DIRECTIVES,
-    formDirectives,
-    HeroDetailComponent,
-  ],
+  styleUrls: const ['app_component.css'],
+  directives: const [CORE_DIRECTIVES, HeroDetailComponent],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
