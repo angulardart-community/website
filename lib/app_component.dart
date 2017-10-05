05fab159b9619016f289d6a4909cf032b81a137a
Patrice Chalin <pchalin@gmail.com>
2017-10-05 13:06:16 -0400

    Sync with 40236a3
    Synced with dart-lang/site-webdev master branch, commit 40236a3:
    https://github.com/dart-lang/site-webdev/tree/40236a3506f601b063c49b9bf1e9c2d7d3513937/examples/ng/doc/toh-4

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -8,18 +8,7 @@ import 'src/hero_service.dart';
 
 @Component(
   selector: 'my-app',
-  template: '''
-      <h1>{{title}}</h1>
-      <h2>My Heroes</h2>
-      <ul class="heroes">
-        <li *ngFor="let hero of heroes"
-          [class.selected]="hero == selectedHero"
-          (click)="onSelect(hero)">
-          <span class="badge">{{hero.id}}</span> {{hero.name}}
-        </li>
-      </ul>
-      <hero-detail [hero]="selectedHero"></hero-detail>
-    ''',
+  templateUrl: 'app_component.html',
   styleUrls: const ['app_component.css'],
   directives: const [CORE_DIRECTIVES, HeroDetailComponent],
   providers: const [HeroService],
