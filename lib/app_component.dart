bb8e04a2df3a01bce6d2f6d557b2febc94861424
Patrice Chalin <pchalin@gmail.com>
2017-10-07 07:39:50 -0400

    Sync with 40ab65a
    Synced with dart-lang/site-webdev master branch, commit 40ab65a:
    https://github.com/dart-lang/site-webdev/tree/40ab65aed3a4162d7be25dd4c35b991370a1b0fb/examples/ng/doc/toh-2

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,25 +1,8 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 
-class Hero {
-  final int id;
-  String name;
-
-  Hero(this.id, this.name);
-}
-
-final mockHeroes = <Hero>[
-  new Hero(11, 'Mr. Nice'),
-  new Hero(12, 'Narco'),
-  new Hero(13, 'Bombasto'),
-  new Hero(14, 'Celeritas'),
-  new Hero(15, 'Magneta'),
-  new Hero(16, 'RubberMan'),
-  new Hero(17, 'Dynama'),
-  new Hero(18, 'Dr IQ'),
-  new Hero(19, 'Magma'),
-  new Hero(20, 'Tornado')
-];
+import 'src/hero.dart';
+import 'src/mock_heroes.dart';
 
 @Component(
   selector: 'my-app',
@@ -29,10 +12,8 @@ final mockHeroes = <Hero>[
 )
 class AppComponent {
   final title = 'Tour of Heroes';
-  final List<Hero> heroes = mockHeroes;
+  List<Hero> heroes = mockHeroes;
   Hero selectedHero;
 
-  void onSelect(Hero hero) {
-    selectedHero = hero;
-  }
+  void onSelect(Hero hero) => selectedHero = hero;
 }
