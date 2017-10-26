f3729924396f2cc5843e786e51e609d94d8cece9
Patrice Chalin <pchalin@gmail.com>
2017-10-26 04:56:21 -0400

    Sync with 2b61f12
    Synced with dart-lang/site-webdev 5-dev branch, commit 2b61f12:
    https://github.com/dart-lang/site-webdev/tree/2b61f1275e18e7d107360ae1ad04d8218d89de77/examples/ng/doc/component-styles

--- lib/src/hero_app_main_component.dart
+++ lib/src/hero_app_main_component.dart
@@ -5,18 +5,15 @@ import 'hero_details_component.dart';
 import 'hero_controls_component.dart';
 import 'quest_summary_component.dart';
 
-@Component(
-    selector: 'hero-app-main',
-    template: '''
+@Component(selector: 'hero-app-main', template: '''
       <quest-summary></quest-summary>
       <hero-details [hero]="hero" [class.active]="hero.active">
         <hero-controls [hero]="hero"></hero-controls>
-      </hero-details>''',
-    directives: const [
-      HeroDetailsComponent,
-      HeroControlsComponent,
-      QuestSummaryComponent
-    ])
+      </hero-details>''', directives: const [
+  HeroDetailsComponent,
+  HeroControlsComponent,
+  QuestSummaryComponent
+])
 class HeroAppMainComponent {
   @Input()
   Hero hero;
