34d002e273c33137e11a8140f26d85ef9a1c5935
Patrice Chalin <pchalin@gmail.com>
2017-11-18 12:48:32 -0500

    Sync with 0bd0c21
    Synced with dart-lang/site-webdev master branch, commit 0bd0c21:
    https://github.com/dart-lang/site-webdev/tree/0bd0c21dd89900a69453177513ff939f131869b1/examples/ng/doc/dependency-injection

--- lib/src/injector_component.dart
+++ lib/src/injector_component.dart
@@ -7,20 +7,30 @@ import 'heroes/hero_service_provider.dart';
 import 'logger_service.dart';
 
 @Component(
-    selector: 'my-injectors',
-    template: '''
+  selector: 'my-injectors',
+  template: '''
       <h2>Other Injections</h2>
       <div id="car">{{car.drive()}}</div>
       <div id="hero">{{hero.name}}</div>
       <div id="rodent">{{rodent}}</div>''',
-    providers: const [Car, Engine, Tires, heroServiceProvider, Logger])
-class InjectorComponent {
+  providers: const [
+    Car,
+    Engine,
+    Tires,
+    heroServiceProvider,
+    Logger,
+  ],
+)
+class InjectorComponent implements OnInit {
   final Injector _injector;
   Car car;
   HeroService heroService;
   Hero hero;
 
-  InjectorComponent(this._injector) {
+  InjectorComponent(this._injector);
+
+  @override
+  void ngOnInit() {
     car = _injector.get(Car);
     heroService = _injector.get(HeroService);
     hero = heroService.getHeroes()[0];
