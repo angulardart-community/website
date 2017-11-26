48821ae5e87bd6915b3ca00969cb0af88d506f00
Patrice Chalin <pchalin@gmail.com>
2017-11-26 11:49:21 -0500

    Sync with a8a83b9
    Synced with dart-lang/site-webdev 5-dev branch, commit a8a83b9:
    https://github.com/dart-lang/site-webdev/tree/a8a83b943e082bc6e3e418d2f0a0460159fe4b00/examples/ng/doc/attribute-directives

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,11 +1,13 @@
 import 'package:angular/angular.dart';
 
+import 'src/auto_id_directive.dart';
 import 'src/highlight_directive.dart';
 
 @Component(
-    selector: 'my-app',
-    templateUrl: 'app_component.html',
-    directives: const [HighlightDirective])
+  selector: 'my-app',
+  templateUrl: 'app_component.html',
+  directives: const [autoIdDirective, HighlightDirective],
+)
 class AppComponent {
   String color;
 }
