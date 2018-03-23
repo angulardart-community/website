150f75285a0d85fd271a3d1d9b91caca464b1fa1
Patrice Chalin <pchalin@gmail.com>
2018-03-23 07:27:10 -0400

    Sync with 6b83dc0
    Synced with dart-lang/site-webdev 5-dev branch, commit 6b83dc0:
    https://github.com/dart-lang/site-webdev/tree/6b83dc011313d69c73e0ed4a338b438d9fcabe20/examples/ng/doc/displaying-data

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -15,7 +15,7 @@ import 'src/hero.dart';
     </ul>
     <p *ngIf="heroes.length > 3">There are many heroes!</p>
   ''',
-  directives: const [coreDirectives],
+  directives: [coreDirectives],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
