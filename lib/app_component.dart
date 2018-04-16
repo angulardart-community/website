ec715832806bf838c90d26543cd224b300139633
Patrice Chalin <pchalin@gmail.com>
2018-04-16 12:52:32 -0400

    Sync with b9d46aa
    Synced with dart-lang/site-webdev master branch, commit b9d46aa:
    https://github.com/dart-lang/site-webdev/tree/b9d46aaae14e72f04838ec1bd9ebca6b9ebd2865/examples/ng/doc/component-styles

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,14 +1,17 @@
 import 'package:angular/angular.dart';
+
 import 'src/hero.dart';
 import 'src/hero_app_main_component.dart';
 
 @Component(
-    selector: 'hero-app',
-    template: '''
-      <h1>Tour of Heroes</h1>
-      <hero-app-main [hero]="hero"></hero-app-main>''',
-    styles: ['h1 { font-weight: normal; }'],
-    directives: [HeroAppMainComponent])
+  selector: 'hero-app',
+  template: '''
+    <h1>Tour of Heroes</h1>
+    <hero-app-main [hero]="hero"></hero-app-main>
+  ''',
+  styles: ['h1 { font-weight: normal; }'],
+  directives: [HeroAppMainComponent],
+)
 class AppComponent {
   Hero hero =
       new Hero('Human Torch', ['Mister Fantastic', 'Invisible Woman', 'Thing']);
