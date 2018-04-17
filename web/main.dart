8376d81da127bc025423cffab22c31864d1e87b5
Patrice Chalin <pchalin@gmail.com>
2018-04-17 14:59:26 -0400

    Sync with ac5a1d4
    Synced with dart-lang/site-webdev master branch, commit ac5a1d4:
    https://github.com/dart-lang/site-webdev/tree/ac5a1d4e6dddbff01b19afea048a938dafae1429/examples/ng/doc/displaying-data

--- web/main.dart
+++ web/main.dart
@@ -1,16 +1,6 @@
 import 'package:angular/angular.dart';
-
-//import 'package:displaying_data/app_component_1.dart' as v1;
-//import 'package:displaying_data/app_component_2.dart' as v2;
-//import 'package:displaying_data/app_component_3.dart' as v3;
-import 'package:displaying_data/app_component.dart';
-
-import 'main.template.dart' as ng;
+import 'package:displaying_data/app_component.template.dart' as ng;
 
 void main() {
-// pick one
-//  bootstrapStatic(v1.AppComponent, [], ng.initReflector);
-//  bootstrapStatic(v2.AppComponent, [], ng.initReflector);
-//  bootstrapStatic(v3.AppComponent, [], ng.initReflector);
-  bootstrapStatic(AppComponent, [], ng.initReflector);
+  runApp(ng.AppComponentNgFactory);
 }
