1f3b7a5d45eda801021246cbf302216955652646
GZGavinZhao <74938940+GZGavinZhao@users.noreply.github.com>
2021-08-19 22:49:36 -0400

    "Auto-commit: update to Angular Version 6"
    Signed-off-by: GZGavinZhao <74938940+GZGavinZhao@users.noreply.github.com>

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,14 +1,19 @@
+// #docregion initial
 @TestOn('browser')
 
 import 'package:angular_test/angular_test.dart';
 import 'package:angular_tour_of_heroes/app_component.dart';
+// #docregion test-bed-and-fixture
 import 'package:angular_tour_of_heroes/app_component.template.dart' as ng;
+// #enddocregion test-bed-and-fixture
 import 'package:test/test.dart';
 
+// #docregion test-bed-and-fixture
 void main() {
   final testBed =
       NgTestBed.forComponent<AppComponent>(ng.AppComponentNgFactory);
   NgTestFixture<AppComponent> fixture;
+  // #enddocregion test-bed-and-fixture
 
   setUp(() async {
     fixture = await testBed.create();
@@ -16,10 +21,13 @@ void main() {
 
   tearDown(disposeAnyRunningTest);
 
+  // #docregion default-test
   test('Default greeting', () {
     expect(fixture.text, 'Hello Angular');
   });
+  // #enddocregion default-test, initial
 
+  // #docregion more-tests
   test('Greet world', () async {
     await fixture.update((c) => c.name = 'World');
     expect(fixture.text, 'Hello World');
@@ -29,4 +37,6 @@ void main() {
     final html = fixture.rootElement.innerHtml;
     expect(html, '<h1>Hello Angular</h1>');
   });
+  // #enddocregion more-tests
+  // #docregion initial, test-bed-and-fixture
 }
