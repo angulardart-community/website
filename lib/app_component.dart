0d1d1ccfe340953084def650c43747851f2c104e
GZGavinZhao <74938940+GZGavinZhao@users.noreply.github.com>
2021-08-19 22:49:41 -0400

    "Auto-commit: update to Angular Version 6"
    Signed-off-by: GZGavinZhao <74938940+GZGavinZhao@users.noreply.github.com>

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -4,11 +4,15 @@ import 'package:angular/angular.dart';
 
 import 'src/hero.dart';
 import 'src/hero_component.dart';
+// #docregion hero-service-import
 import 'src/hero_service.dart';
+// #enddocregion hero-service-import
 
 @Component(
   selector: 'my-app',
+  // #docregion template
   templateUrl: 'app_component.html',
+  // #enddocregion template
   styleUrls: ['app_component.css'],
   directives: [coreDirectives, HeroComponent],
   providers: [ClassProvider(HeroService)],
@@ -21,9 +25,11 @@ class AppComponent implements OnInit {
 
   AppComponent(this._heroService);
 
+  // #docregion _getHeroes
   Future<void> _getHeroes() async {
     heroes = await _heroService.getAll();
   }
+  // #enddocregion _getHeroes
 
   void ngOnInit() => _getHeroes();
 
